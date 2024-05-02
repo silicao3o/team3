fe4c8f7cfb1ec521d021883a467c0b08ea06e028
MoJun00 <167170478+MoJun00@users.noreply.github.com>
2024-05-02 17:27:45 +0900

    test1

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -235,6 +235,13 @@ public class CampManagementApplication {
     private static void inquireRoundGradeBySubject() {
         String studentId = getStudentId(); // 관리할 수강생 고유 번호
         // 기능 구현 (조회할 특정 과목)
+
+        System.out.print("\n조회할 과목을 입력해주세요 : ");
+        String subjectName = sc.next();
+
+
+        //subjectStore
+
         System.out.println("회차별 등급을 조회합니다...");
         // 기능 구현
         System.out.println("\n등급 조회 성공!");
